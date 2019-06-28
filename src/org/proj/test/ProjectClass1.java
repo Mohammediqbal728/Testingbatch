85124ed6055eba11e58cf94f0004401a1cee3963
Mohammediqbal728 <Mohammediqbal728@gmail.com>
2019-06-28 09:37:51 -0700

    New code added

---
+++ src/org/proj/test/ProjectClass1.java
@@ -0,0 +1,33 @@
+package org.proj.test;
+
+
+public class ProjectClass1 {
+	public class CompanyDetails {
+		public void companyName() {
+			
+			System.out.println("oracle");
+			
+		}
+	public void companyaddress() {
+		System.out.println("Banglore");
+		
+	}
+	public void companyLocation() 
+
+	{
+		System.out.println("new");
+		
+
+	}
+	public void companyEmployee() {
+		System.out.println("300");
+
+	}
+	public void main(String[] args) {
+		CompanyDetails n=new CompanyDetails();
+		n.companyName();
+		n.companyaddress();
+		n.companyLocation();
+		n.companyEmployee();
+		ProjectClass1 z =new ProjectClass1();
+	}}}
