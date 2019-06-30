1ae5c3e1b4bad67eda0da81046f4bd4ae08c445c
Mohammediqbal728 <mohammediqbal728@gmail.com>
2019-06-29 22:03:05 -0700

    updated prg

---
+++ src/org/proj/test/Array.java
@@ -0,0 +1,30 @@
+package org.proj.test;
+
+public class Array {
+	public static void main(String[] args) {
+		int a[]=new int[5];
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		a[3]=40;
+		a[4]=50;
+		a[3]=90;
+		
+
+		System.out.println(a[2]);
+int l=a.length;
+System.out.println(l);
+for (int i=0;i<a.length;i++) {
+	System.out.println(a[i]);
+	
+}
+for(int x:a)
+{
+	System.out.println(x);
+}
+		
+	}
+
+
+
+}
---
+++ src/org/proj/test/EvenNum.java
@@ -0,0 +1,14 @@
+package org.proj.test;
+
+public class EvenNum {
+	public static void main(String[] args) {
+		for(int i=1;i<=30;i++) {
+			if(i%2==0){
+				System.out.println(i);
+			}
+		}
+		
+	}
+	
+
+}
