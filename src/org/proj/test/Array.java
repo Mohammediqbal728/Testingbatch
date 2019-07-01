86b906bf11d4b1ecf9e2ed8cea86c07e736b634c
Mohammediqbal728 <mohammediqbal728@gmail.com>
2019-07-01 08:12:18 +0530

    Array prg updayted

--- src/org/proj/test/Array.java
+++ src/org/proj/test/Array.java
@@ -2,14 +2,14 @@ package org.proj.test;
 
 public class Array {
 	public static void main(String[] args) {
-		int a[]=new int[5];
+		int a[]=new int[6];
 		a[0]=10;
 		a[1]=20;
 		a[2]=30;
 		a[3]=40;
 		a[4]=50;
 		a[3]=90;
-		
+		a[5]=45;		
 
 		System.out.println(a[2]);
 int l=a.length;
